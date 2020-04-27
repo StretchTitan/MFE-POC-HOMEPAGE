2bb8f106e05d661c05bda79a09d5d985cd02e513
Christopher Breit <christopher.breit@charter.com>
2020-04-27 15:35:20 -0600

    updates to make this app a web component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MFE-POC-HOMEPAGE';
+
+  constructor(private router: Router) {
+    router.initialNavigation();
+    // Manually triggering initial navigation
+  }
 }
