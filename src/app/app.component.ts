28617073ab051f9fde971edaa884d35d4d76a836
Christopher Breit <christopher.breit@charter.com>
2020-05-08 13:54:11 -0600

    Added Sub App Router Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  @Input() state;
+export class AppComponent implements OnChanges {
+  @Input() nameState;
+  @Input() routerState;
   @Output() msg = new EventEmitter<any>();
+
+  constructor(private router: Router) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.routerState && changes.routerState.currentValue.startsWith('/home')) {
+      this.router.navigate([changes.routerState.currentValue]);
+    }
+  }
 
   clearName() {
     this.msg.emit({ action: 'clearName' });
